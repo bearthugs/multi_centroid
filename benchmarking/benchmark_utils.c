c9099f82ed577c5cdd30c6083cbf3b05a453e696
bearthugs <claretian1405@gmail.com>
2025-09-25 23:08:24 +1000

    Added benchmarking files

---
+++ benchmarking/benchmark_utils.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+// Load the data files
+// Returns: float pointer to all vectors (malloc'ed, caller frees)
---
+++ benchmarking/faiss_wrapper.cpp
@@ -0,0 +1,8 @@
+#include "faiss_wrapper.h"
+#include <IndexHNSW.h>
+#include <IndexFlat.h>
+#include <IndexIVFFlat.h>
+#include <index_io.h>
+#include <MetaIndexes.h>
+#include <vector>
+#include <iostream>
