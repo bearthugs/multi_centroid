e12a8eef156119a9772e1e5efdf3d7cb8c5350db
bearthugs <claretian1405@gmail.com>
2025-09-25 23:24:08 +1000

    Added HNSW in wrapper

--- benchmarking/faiss_wrapper.cpp
+++ benchmarking/faiss_wrapper.cpp
@@ -6,3 +6,36 @@
 #include <MetaIndexes.h>
 #include <vector>
 #include <iostream>
+
+struct FaissIndexWrapper {
+    faiss::IndexHNSWFlat* index;
+};
+
+extern "C" {
+
+FaissIndexHandle faiss_create_hnsw_index(int d) {
+    auto wrapper = new FaissIndexWrapper();
+    // Use IndexHNSWFlat with L2 metric
+    wrapper->index = new faiss::IndexHNSWFlat(d, 32);
+    wrapper->index->hnsw.efConstruction = 40;
+    wrapper->index->hnsw.efSearch = 16;
+    return (FaissIndexHandle)wrapper;
+}
+
+void faiss_add_vectors(FaissIndexHandle handle, int n, float* vectors) {
+    FaissIndexWrapper* wrapper = (FaissIndexWrapper*)handle;
+    wrapper->index->add(n, vectors);
+}
+
+void faiss_search(FaissIndexHandle handle, int nq, float* queries, int k, float* distances, int64_t* labels) {
+    FaissIndexWrapper* wrapper = (FaissIndexWrapper*)handle;
+    wrapper->index->search(nq, queries, k, distances, labels);
+}
+
+void faiss_free_index(FaissIndexHandle handle) {
+    FaissIndexWrapper* wrapper = (FaissIndexWrapper*)handle;
+    delete wrapper->index;
+    delete wrapper;
+}
+
+} // extern "C"
--- benchmarking/faiss_wrapper.h
+++ benchmarking/faiss_wrapper.h
@@ -1,1 +1,35 @@
-// help lmao
+#ifndef FAISS_WRAPPER_H
+#define FAISS_WRAPPER_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef void* FaissIndex;
+
+// Create different index types
+FaissIndex faiss_create_flat_index(int dim);
+FaissIndex faiss_create_hnsw_index(int dim, int M);
+FaissIndex faiss_create_ivf_flat_index(int dim, int nlist);
+FaissIndex faiss_create_ivf_hnsw_index(int dim, int nlist, int M);
+FaissIndex faiss_create_ivf_pq_index(int dim, int nlist, int m, int nbits);
+
+// Train IVF / IVF+PQ indexes before adding vectors
+void faiss_train_ivf_index(FaissIndex index, float* data, int n, int dim);
+
+// Add vectors
+void faiss_add_vectors(FaissIndex index, float* data, int n, int dim);
+
+// Search vectors
+void faiss_search_vectors(FaissIndex index, float* queries, int n_query, int dim, int k, int64_t* labels, float* distances);
+
+// Free index memory
+void faiss_free_index(FaissIndex index);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // FAISS_WRAPPER_H
